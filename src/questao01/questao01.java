22a5b0649d41387b96fbd4abb704a396669cc83a
Ariana Natalia <natalia1029@users.noreply.github.com>
2025-08-02 18:26:13 -0300

    Resolução Questao01

---
+++ src/questao01/questao01.java
@@ -0,0 +1,26 @@
+package questao01;
+
+import java.util.Scanner;
+public class questao01 {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        int numero = scanner.nextInt();
+        System.out.println("Informe outro numero: ");
+        int numero2 = scanner.nextInt();
+
+        if (numero < numero2){
+           System.out.println("O maior número é " +numero2);
+        }
+        else {
+            System.out.println("O maior número é "+ numero);
+
+        }
+
+        }
+
+
+    }
+    
+
